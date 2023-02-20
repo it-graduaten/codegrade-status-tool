df0ef16d43957c756e45f7d59f5656ce87f15fdb
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-02-20 11:38:42 +0100

    Rework courses

--- frontend/static-data/courses.ts
+++ frontend/static-data/courses.ts
@@ -2,138 +2,67 @@ const courses = [
     {
         codegradeId: 3711,
         name: 'Inleiding tot Programmeren - G, L & T',
-        chapters: [{
-            number: "01", name: "Sequentie", file: "seqssuence", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "02", name: "Selectie deel 1", file: "selection-part-1", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "03", name: "Selectie deel 2", file: "selection-part-2", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "04", name: "For loop", file: "for-loop", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "05", name: "Do while", file: "do-while", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "06", name: "While do", file: "while-do", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "07", name: "Collecties", file: "collections", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "08", name: "Methodes", file: "methods", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "09", name: "Stringmethodes", file: "stringmethods", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "10", name: "Bestanden", file: "files", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },]
+        chapters: [
+            { number: "01", name: "Sequentie" },
+            { number: "02", name: "Selectie deel 1" },
+            { number: "03", name: "Selectie deel 2" },
+            { number: "04", name: "For loop" },
+            { number: "05", name: "Do while" },
+            { number: "06", name: "While do" },
+            { number: "07", name: "Collecties" },
+            { number: "08", name: "Methodes" },
+            { number: "09", name: "Stringmethodes" },
+            { number: "10", name: "Bestanden" }
+        ]
     },
     {
         codegradeId: 3719,
         name: 'Inleiding tot Programmeren - GA & LA',
-        chapters: [{
-            number: "01", name: "Sequentie", file: "seqssuence", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "02", name: "Selectie deel 1", file: "selection-part-1", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "03", name: "Selectie deel 2", file: "selection-part-2", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "04", name: "For loop", file: "for-loop", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "05", name: "Do while", file: "do-while", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "06", name: "While do", file: "while-do", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "07", name: "Collecties", file: "collections", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "08", name: "Methodes", file: "methods", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "09", name: "Stringmethodes", file: "stringmethods", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },
-        {
-            number: "10", name: "Bestanden", file: "files", assignments: [], deadline: null,
-            mandatoryAssignments: ["01", "02", "03", "04", "05"]
-        },]
+        chapters: [
+            { number: "01", name: "Sequentie" },
+            { number: "02", name: "Selectie deel 1" },
+            { number: "03", name: "Selectie deel 2" },
+            { number: "04", name: "For loop" },
+            { number: "05", name: "Do while" },
+            { number: "06", name: "While do" },
+            { number: "07", name: "Collecties" },
+            { number: "08", name: "Methodes" },
+            { number: "09", name: "Stringmethodes" },
+            { number: "10", name: "Bestanden" }
+        ]
     },
     {
         codegradeId: 4041,
         name: "Objectgeoriënteerd programmeren -G, L & T",
-        chapters: [{
-            number: "01", name: "Objectgeoriënteerd programmeren deel 1", deadline: null,
-            mandatoryAssignments: ["01", "02"]
-        },
-        {
-            number: "02", name: "Objectgeoriënteerd programmeren deel 2", deadline: null,
-            mandatoryAssignments: ["01", "02"]
-        },
-        {
-            number: "03", name: "Statische klassen", deadline: null,
-            mandatoryAssignments: ["01", "02"]
-        },
-        {
-            number: "04", name: "Overerving deel 1", deadline: null,
-            mandatoryAssignments: ["01", "02"]
-        },
-        {
-            number: "05", name: "Overerving deel 2", deadline: null,
-            mandatoryAssignments: ["01", "02"]
-        },
-        {
-            number: "06", name: "Lijsten en OO", deadline: null,
-            mandatoryAssignments: ["01", "02"]
-        },
-        {
-            number: "07", name: "Enkelvoudige relaties", deadline: null,
-            mandatoryAssignments: ["01", "02"]
-        },
-        {
-            number: "08", name: "Unit testing", deadline: null,
-            mandatoryAssignments: []
-        },
-        {
-            number: "09", name: "Exception handling", deadline: null,
-            mandatoryAssignments: ["01", "02"]
-        },
-        {
-            number: "10", name: "WPF deel 1", deadline: null,
-            mandatoryAssignments: []
-        },
-        {
-            number: "11", name: "WPF deel 2", deadline: null,
-            mandatoryAssignments: []
-        }
+        chapters: [
+            { number: "01", name: "Objectgeoriënteerd programmeren deel 1" },
+            { number: "02", name: "Objectgeoriënteerd programmeren deel 2" },
+            { number: "03", name: "Statische klassen" },
+            { number: "04", name: "Overerving deel 1" },
+            { number: "05", name: "Overerving deel 2" },
+            { number: "06", name: "Lijsten en OO" },
+            { number: "07", name: "Enkelvoudige relaties" },
+            { number: "08", name: "Unit testing" },
+            { number: "09", name: "Exception handling" },
+            { number: "10", name: "WPF deel 1" },
+            { number: "11", name: "WPF deel 2" }
+        ]
+    },
+    {
+        codegradeId: 4059,
+        name: "Objectgeoriënteerd programmeren - GA & LA",
+        chapters: [
+            { number: "01", name: "Objectgeoriënteerd programmeren deel 1" },
+            { number: "02", name: "Objectgeoriënteerd programmeren deel 2" },
+            { number: "03", name: "Statische klassen" },
+            { number: "04", name: "Overerving deel 1" },
+            { number: "05", name: "Overerving deel 2" },
+            { number: "06", name: "Lijsten en OO" },
+            { number: "07", name: "Enkelvoudige relaties" },
+            { number: "08", name: "Unit testing" },
+            { number: "09", name: "Exception handling" },
+            { number: "10", name: "WPF deel 1" },
+            { number: "11", name: "WPF deel 2" }
         ]
     }
 ]
